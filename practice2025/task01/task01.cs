d61d346b7ce00ece5c2504d17a67b3df0465961e
sofyaperederina <sonya.perederina@mail.ru>
2025-06-24 15:58:17 +0600

    init task01

---
+++ practice2025/task01/task01.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace task01
+{
+    public static class StringExtensions
+    {
+        public static bool IsPalindrome(this string str)
+        {
+            if (string.IsNullOrEmpty(str)) return false;
+            var lowStr = str.Where(symb => !char.IsPunctuation(symb) && !char.IsWhiteSpace(symb)).Select(char.ToLower).ToArray();
+
+            return lowStr.SequenceEqual(lowStr.Reverse());
+        }
+    }
+}
