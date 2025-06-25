63e64a1fa8ca8713b1ba7159912d941e70d75401
sofyaperederina <sonya.perederina@mail.ru>
2025-06-25 20:05:50 +0600

    fix: warning CS8618

--- practice2025/task02/Class1.cs
+++ practice2025/task02/Class1.cs
@@ -3,9 +3,9 @@ namespace task02
 {
     public class Student
     {
-        public string Name { get; set; }
-        public string Faculty { get; set; }
-        public List<int> Grades { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Faculty { get; set; } = string.Empty;
+        public List<int> Grades { get; set; } = new List<int>();
     }
 
     public class StudentService
