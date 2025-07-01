5006d949c40d42d56437e4479b5a59be4755eaac
sofyaperederina <sonya.perederina@mail.ru>
2025-07-01 14:24:57 +0600

    init: task08

---
+++ practice2025/CommandRunner/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using CommandLib;
+namespace Command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string searchDllPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "FileSystemCommands.dll");
+            Assembly assembly = Assembly.LoadFrom(searchDllPath);
+
+            Type typeDirectorySize = assembly.GetType("FileSystemCommands.DirectorySizeCommand")!;
+            ICommand directorySizeInstance = (ICommand)Activator.CreateInstance(typeDirectorySize, Directory.GetCurrentDirectory())!;
+
+            directorySizeInstance.Execute();
+
+            Type typeSearchFiles = assembly.GetType("FileSystemCommands.FindFilesCommand")!;
+            ICommand searchInstance = (ICommand)Activator.CreateInstance(typeSearchFiles, Directory.GetCurrentDirectory(), "*.cs")!;
+
+            searchInstance.Execute();
+
+        }
+    }
+}
---
+++ practice2025/task08tests/UnitTest1.cs
@@ -0,0 +1,41 @@
+using FileSystemCommands;
+
+namespace task08tests
+{
+    public class FileSystemCommandsTests
+    {
+        [Fact]
+        public void DirectorySizeCommand_ShouldCalculateSize()
+        {
+            var testDir = Path.Combine(Path.GetTempPath(), "TestDir");
+            Directory.CreateDirectory(testDir);
+            File.WriteAllText(Path.Combine(testDir, "test1.txt"), "Hello");
+            File.WriteAllText(Path.Combine(testDir, "test2.txt"), "World");
+
+            var infOfTestDir = new DirectoryInfo(testDir);
+            long sizeOfTestDir = infOfTestDir.GetFiles().Select(f => f.Length).Sum();
+
+            var command = new DirectorySizeCommand(testDir);
+            command.Execute();
+            
+            Assert.Equal(sizeOfTestDir, command.SizeOfDirectory);
+            Directory.Delete(testDir, true);
+        }
+
+        [Fact]
+        public void FindFilesCommand_ShouldFindMatchingFiles()
+        {
+            var testDir = Path.Combine(Path.GetTempPath(), "TestDir");
+            Directory.CreateDirectory(testDir);
+            File.WriteAllText(Path.Combine(testDir, "file1.txt"), "Text");
+            File.WriteAllText(Path.Combine(testDir, "file2.log"), "Log");
+
+            var command = new FindFilesCommand(testDir, "*.txt");
+            command.Execute();
+
+            Assert.Single(command.FilesWithMask);
+            Assert.Contains("file1.txt", command.FilesWithMask.Select(f => f.Name));
+            Directory.Delete(testDir, true);
+        }
+    }
+}
