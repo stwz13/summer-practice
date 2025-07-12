d6377bba5781d42702dab3dedabccdb986d83753
sofyaperederina <sonya.perederina@mail.ru>
2025-07-12 09:49:52 +0600

    fix: cs8618

--- practice2025/task13/Class1.cs
+++ practice2025/task13/Class1.cs
@@ -7,16 +7,16 @@ namespace task13
 {
     public class Subject
     {
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public int Grade { get; set; }
     }
 
     public class Student
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; }= string.Empty;
         public DateTime BirthDate { get; set; }
-        public List<Subject> Grades { get; set; }
+        public List<Subject> Grades { get; set; } = new List<Subject>();
     }
 
     public class DateTimeJsonConverter : JsonConverter<DateTime>
