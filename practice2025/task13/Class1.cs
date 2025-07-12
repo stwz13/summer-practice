2b73859b99972691b5ca642dfad6062bdfdd9a19
sofyaperederina <sonya.perederina@mail.ru>
2025-07-12 09:46:15 +0600

    init: task13

---
+++ practice2025/task13/Class1.cs
@@ -0,0 +1,76 @@
+﻿using System.Globalization;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+
+namespace task13
+{
+    public class Subject
+    {
+        public string Name { get; set; }
+        public int Grade { get; set; }
+    }
+
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public List<Subject> Grades { get; set; }
+    }
+
+    public class DateTimeJsonConverter : JsonConverter<DateTime>
+    {
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+            DateTime.ParseExact(reader.GetString()!, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options) =>
+            writer.WriteStringValue(value.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture));
+
+    }
+
+    public class Serilazator
+    {
+        public static string Serilaze(Student student)
+        {
+
+            var options = new JsonSerializerOptions
+            {
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+                Converters = { new DateTimeJsonConverter() },
+                WriteIndented = true,
+            };
+            return JsonSerializer.Serialize(student, options);
+            
+        }
+    }
+    public class Deserializator
+    {
+        public static Student DeserializationFromString(string input)
+        {
+            var options = new JsonSerializerOptions
+            {
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+                Converters = { new DateTimeJsonConverter() },
+                WriteIndented = true,
+            };
+
+            Student student = JsonSerializer.Deserialize<Student>(input, options)!;
+
+            if (student == null) throw new Exception("Ошибка десериализации");
+
+            if (!CheckForNull(student)) throw new Exception("Данные содержат пустую строку или значение null");
+
+            if (DateTime.Today < student.BirthDate || student.BirthDate.Year < 1900) throw new Exception("Некорректная дата");
+
+            if (student.Grades.Select(subj => subj.Grade <= 0).Contains(true)) throw new Exception("Оценка должна быть натуральным числом");
+
+            return student;
+        }
+        public static bool CheckForNull(Student student) => !string.IsNullOrEmpty(student.FirstName)
+            && !string.IsNullOrEmpty(student.LastName) && (student.Grades != null)
+            && !student.Grades.Select(subj => string.IsNullOrEmpty(subj.Name)).Contains(true);
+
+    }
+}
---
+++ practice2025/task13tests/UnitTest1.cs
@@ -0,0 +1,205 @@
+﻿using task13;
+using ConsoleSerializatorApp;
+
+namespace task13tests
+{
+    public class UnitTest1
+    {
+        string testJsonStudent = @"{
+          ""FirstName"": ""Петр"", ""LastName"": ""Петров"", ""BirthDate"": ""05/05/2005"", ""Grades"": [
+            {
+              ""Name"": ""Математика"",
+              ""Grade"": 5
+            },
+            {
+              ""Name"": ""Метрология"",
+              ""Grade"": 5
+            },
+            {
+              ""Name"": ""Английский"",
+              ""Grade"": 5
+            }
+          ]
+        }";
+        Student testStudent = new Student()
+        {
+                FirstName = "Петр",
+                LastName = "Петров",
+                BirthDate = new DateTime(2005, 5, 5),
+                Grades = new List<Subject>
+                {
+                    new Subject{Name = "Математика", Grade = 5 },
+                    new Subject{Name = "Метрология", Grade = 5 },
+                    new Subject{Name = "Английский", Grade = 5 }
+                }
+            };
+        Student wrongTestStudent = new Student()
+        {
+            FirstName = null,
+            LastName = "Петров",
+            BirthDate = new DateTime(2005, 5, 5),
+            Grades = new List<Subject>
+                {
+                    new Subject{Name = null, Grade = 5 },
+                    new Subject{Name = "Метрология", Grade = 5 },
+                    new Subject{Name = "Английский", Grade = 5 }
+                }
+        };
+        [Fact]
+        public void isValidMethod_ReturnsTrueWithCorrectData() => 
+            Assert.True(Deserializator.CheckForNull(testStudent));
+        
+
+        [Fact]
+        public void isValidMethod_ReturnsTrueWithIncorrectFirstName() => 
+            Assert.False(Deserializator.CheckForNull(wrongTestStudent));
+        
+        [Fact]
+        public void isValidMethod_ReturnsTrueWithSubjectsName() =>
+            Assert.False(Deserializator.CheckForNull(wrongTestStudent));
+        
+        [Fact]
+        public void Deserialization_ReturnsNotNullWithCorrectStudentsFields()
+        {
+            Student student = Deserializator.DeserializationFromString(testJsonStudent)!;
+
+            Assert.True(student != null && 
+                student.FirstName != null &&
+                student.LastName != null && 
+                student.Grades != null);
+
+        }
+
+        [Fact]
+        public void Deserialization_ReturnsCorrectFields()
+        {
+            Student student = Deserializator.DeserializationFromString(testJsonStudent)!;
+
+            Assert.Equal(student.FirstName, testStudent.FirstName);
+            Assert.Equal(student.LastName, testStudent.LastName);
+            Assert.Equal(student.BirthDate, testStudent.BirthDate);
+            Assert.Equal(3, student.Grades.Count);
+            Assert.Equal("Математика", student.Grades[0].Name);
+            Assert.Equal("Метрология", student.Grades[1].Name);
+            Assert.Equal("Английский", student.Grades[2].Name);
+        }
+
+        [Fact]
+        public void Deserialization_ReturnsExceptionWithIncorrectData()
+        {
+            string wrongTestJsonStudent = @"{
+                  ""FirstName"": """", ""LastName"": """", ""BirthDate"": ""05/05/2005"", ""Grades"": [
+                    {
+                      ""Name"": """",
+                      ""Grade"": 5
+                    },
+                    {
+                      ""Name"": """",
+                      ""Grade"": 5
+                    },
+                    {
+                      ""Name"": """",
+                      ""Grade"": 5
+                    }
+                  ]
+            }";
+            var exception = Assert.Throws<Exception>(() => Deserializator.DeserializationFromString(wrongTestJsonStudent));
+            Assert.Contains("Данные содержат пустую строку или значение null", exception.Message);
+        }
+        [Fact]
+        public void Deserialization_ReturnsExceptionWithWrongData()
+        {
+            string wrongDataJsonStudent = @"{
+                  ""FirstName"": ""Петр"", ""LastName"": ""Петров"", ""BirthDate"": ""05/05/1895"", ""Grades"": [
+                    {
+                      ""Name"": ""Математика"",
+                      ""Grade"": 5
+                    },
+                    {
+                      ""Name"": ""Метрология"",
+                      ""Grade"": 5
+                    },
+                    {
+                      ""Name"": ""Английский"",
+                      ""Grade"": 5
+                    }
+                  ]
+             }";
+
+            var exception = Assert.Throws<Exception>(() => Deserializator.DeserializationFromString(wrongDataJsonStudent));
+            Assert.Contains("Некорректная дата", exception.Message);
+
+        }
+        [Fact]
+        public void Deserialization_ReturnsExceptionWithWrongGrades()
+        {
+            string wrongGradesJsonStudent = @"{
+                  ""FirstName"": ""Петр"", ""LastName"": ""Петров"", ""BirthDate"": ""05/05/1995"", ""Grades"": [
+                    {
+                      ""Name"": ""Математика"",
+                      ""Grade"": -3
+                    },
+                    {
+                      ""Name"": ""Метрология"",
+                      ""Grade"": 5
+                    },
+                    {
+                      ""Name"": ""Английский"",
+                      ""Grade"": 5
+                    }
+                  ]
+             }";
+
+            var exception = Assert.Throws<Exception>(() => Deserializator.DeserializationFromString(wrongGradesJsonStudent));
+            Assert.Contains("Оценка должна быть натуральным числом", exception.Message);
+
+        }
+
+        [Fact]            
+        public void Serializator_ReturnCorrectField()
+        {
+            string jsonStudent = Serilazator.Serilaze(testStudent);
+
+            Student student = Deserializator.DeserializationFromString(jsonStudent)!;
+
+            Assert.Equal(student.FirstName, testStudent.FirstName);
+            Assert.Equal(student.LastName, testStudent.LastName);
+            Assert.Equal(student.BirthDate, testStudent.BirthDate);
+            Assert.Equal(3, student.Grades.Count);
+            Assert.Equal("Математика", student.Grades[0].Name);
+            Assert.Equal("Метрология", student.Grades[1].Name);
+            Assert.Equal("Английский", student.Grades[2].Name);
+        }
+
+        [Fact]
+        public void SaveSerializationInFile_SavesDataInFile()
+        {
+            string filePath = "studentInfo.json";
+
+            JsonFileWork.SaveSerializationInFile(testStudent, filePath);
+            Assert.True(File.Exists(filePath));
+
+            string fileData = File.ReadAllText(filePath);
+            Student student = Deserializator.DeserializationFromString(fileData)!;
+            Assert.NotNull(student);
+        }
+        [Fact]
+        public void DeserelizationFromFile_ReturnsCorrectData()
+        {
+            string filePath = "studentInfo.json";
+            JsonFileWork.SaveSerializationInFile(testStudent, filePath);
+
+            Student student = JsonFileWork.DeserelizationFromFile(filePath)!;
+
+            Assert.Equal(student.FirstName, testStudent.FirstName);
+            Assert.Equal(student.LastName, testStudent.LastName);
+            Assert.Equal(student.BirthDate, testStudent.BirthDate);
+            Assert.Equal(3, student.Grades.Count);
+            Assert.Equal("Математика", student.Grades[0].Name);
+            Assert.Equal("Метрология", student.Grades[1].Name);
+            Assert.Equal("Английский", student.Grades[2].Name);
+
+        }
+
+    }
+}
